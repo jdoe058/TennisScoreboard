5007b2115afaa10418af95beab29659ba85c861b
Zheka Doe <jdoe058@gmail.com>
2024-12-20 15:11:25 +0300

    feat: /new-match

--- src/main/java/edu/zhekadoe/tennisscoreboard/utils/PlayerName.java
+++ src/main/java/edu/zhekadoe/tennisscoreboard/utils/PlayerName.java
@@ -6,13 +6,4 @@ import lombok.experimental.UtilityClass;
 public class PlayerName {
     public final String PATTERN = "[A-Za-z]. [A-Za-z]+";
     public final String EXAMPLE = "n. surname";
-
-    /**
-     * Проверяет, соответствует ли имя игрока заданному шаблону.
-     * @param name Имя игрока для проверки.
-     * @return true, если имя некорректно; false в противном случае.
-     */
-    public boolean hasError(String name) {
-        return name == null || !name.matches(PATTERN);
-    }
 }
--- src/main/webapp/WEB-INF/jsp/error.jsp
+++ src/main/webapp/WEB-INF/jsp/error.jsp
@@ -5,7 +5,7 @@
   Time: 04:08
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <c:if test="${not empty requestScope.error}">
     <p style="color: darkred">${requestScope.error}</p>
