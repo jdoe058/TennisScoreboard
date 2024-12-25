08de4acf389759ecbedd8dca9a7d1ade1c8bd7b7
Zheka Doe <jdoe058@gmail.com>
2024-12-25 16:43:27 +0300

    fix: MatchContextListener

--- src/main/java/edu/zhekadoe/tennisscoreboard/MatchContextListener.java
+++ src/main/java/edu/zhekadoe/tennisscoreboard/MatchContextListener.java
@@ -5,6 +5,7 @@ import edu.zhekadoe.tennisscoreboard.model.Player;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
+import lombok.experimental.UtilityClass;
 import org.hibernate.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,16 +14,9 @@ import java.util.List;
 import java.util.Random;
 
 @WebListener
-public class ContextListener implements ServletContextListener {
-    Logger logger = LoggerFactory.getLogger(ContextListener.class);
+public class MatchContextListener implements ServletContextListener {
+    Logger logger = LoggerFactory.getLogger(MatchContextListener.class);
     private static final int INIT_NUMBER_OF_MATCHES = 50;
-    private static final List<String> INIT_PLAYER_NAMES = List.of(
-            "A. Smith", "B. Johnson", "C. Brown",
-            "D. Davis", "E. Miller", "F. Wilson",
-            "G. Moore", "H. Taylor", "I. Anderson",
-            "J. Thomas"
-    );
-    private final Random random = new Random();
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
@@ -31,10 +25,9 @@ public class ContextListener implements ServletContextListener {
         try (Session session = HibernateUtil
                 .getSessionFactory()
                 .openSession()) {
-
             session.beginTransaction();
             for (int i = 0; i < INIT_NUMBER_OF_MATCHES; i++) {
-                session.persist(createMatch(session));
+                RandomMatch.create(session);
             }
             session.getTransaction().commit();
             logger.info("Successfully created {} matches.", INIT_NUMBER_OF_MATCHES);
@@ -43,37 +36,47 @@ public class ContextListener implements ServletContextListener {
         }
     }
 
-    private Match createMatch(Session session) {
-        String nameOne = getRandomPlayerName();
-        String nameTwo;
-
-        do {
-            nameTwo = getRandomPlayerName();
-        } while (nameTwo.equals(nameOne));
-
-        Player playerOne = getOrCreatePlayer(session, nameOne);
-        Player playerTwo = getOrCreatePlayer(session, nameTwo);
-
-        return new Match(playerOne, playerTwo, random.nextBoolean());
-    }
-
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         logger.info("Shutting down Hibernate session factory.");
         HibernateUtil.shutdown();
     }
+}
+
+@UtilityClass
+class RandomMatch {
+    private static final List<String> INIT_PLAYER_NAMES = List.of(
+            "A. Smith", "B. Johnson", "C. Brown",
+            "D. Davis", "E. Miller", "F. Wilson",
+            "G. Moore", "H. Taylor", "I. Anderson",
+            "J. Thomas"
+    );
+    private final Random random = new Random();
+
+    public void create(Session session) {
+        String nameOne = getRandomPlayerName();
+        String nameTwo = getRandomPlayerName(nameOne);
+        Player playerOne = getOrCreatePlayer(session, nameOne);
+        Player playerTwo = getOrCreatePlayer(session, nameTwo);
+        session.persist(new Match(playerOne, playerTwo, random.nextBoolean()));
+    }
 
     private String getRandomPlayerName() {
-        return INIT_PLAYER_NAMES.get(
-                random.nextInt(INIT_PLAYER_NAMES.size()));
+        return INIT_PLAYER_NAMES.get(random.nextInt(INIT_PLAYER_NAMES.size()));
+    }
+
+    private String getRandomPlayerName(String name) {
+        String result;
+        do {
+            result = getRandomPlayerName();
+        } while (result.equals(name));
+        return result;
     }
 
     private Player getOrCreatePlayer(Session session, String playerName) {
-        Player player = session.createQuery(
-                "FROM Player WHERE name = :name", Player.class)
+        Player player = session.createQuery("FROM Player WHERE name = :name", Player.class)
                 .setParameter("name", playerName)
                 .uniqueResult();
-
         if (player == null) {
             player = new Player(playerName);
             session.persist(player);
