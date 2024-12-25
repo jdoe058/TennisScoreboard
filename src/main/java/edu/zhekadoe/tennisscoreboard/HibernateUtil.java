d25f321b9c47b2782358dcfb6ccdc8c53c2e3970
Zheka Doe <jdoe058@gmail.com>
2024-12-25 11:16:10 +0300

    fix: HibernateUtil

--- src/main/java/edu/zhekadoe/tennisscoreboard/HibernateUtil.java
+++ src/main/java/edu/zhekadoe/tennisscoreboard/HibernateUtil.java
@@ -1,7 +1,6 @@
 package edu.zhekadoe.tennisscoreboard;
 
 import lombok.Getter;
-import lombok.Value;
 import lombok.experimental.UtilityClass;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
